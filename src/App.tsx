00308c23db49aeb76bab911a7026b22bce168ce2
Kimerie Green <kimerie.green@gmail.com>
2025-08-26 19:57:20 +0000

    nasa image

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,116 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { DateTime } from "luxon";
+import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./components/ui/card";
+import {
+  ChevronRightIcon,
+  ChevronDoubleLeftIcon,
+  ChevronLeftIcon,
+} from "@heroicons/react/16/solid";
 import "./App.css";
 
+const API_KEY = "O10aHgUThs9QtlSnv0LJWbM2gNfnYbnRUGwsYzpO";
+
+const response = {
+  copyright: "\nAndrea Girones\n",
+  date: "2025-08-26",
+  explanation:
+    "What's hovering above the Sun?  A solar prominence. A prominence is a crest of hot gas expelled from the Sun's surface that is held aloft by the Sun's magnetic field.  Prominences can last for days, can suddenly explode into space, or just fall back to the Sun.  What decides a prominence's fate is how the Sun's complex magnetic field changes -- the field's direction can act like an offramp for trapped solar particles. The 3-second (repeating) time-lapse featured video was captured earlier this month from Ottawa, Ontario, Canada. It shows the development of a larger-than-Earth prominence as it appears to leak solar plasma back to the Sun, over the course of an hour. What is unusual is that the prominence appears to hover -- more simple and typical prominences form magnetic loops that connect back to the surface.  Many hours after this video ended, the hovering prominence disintegrated back into the Sun.    Explore the Universe: Random APOD Generator",
+  media_type: "other",
+  service_version: "v1",
+  title: "A Leaky Solar Prominence",
+  url: "https://api.nasa.gov/assets/img/general/apod.jpg",
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [currentDate, setCurrentDate] = useState(DateTime.now());
+  const [today, setToday] = useState(DateTime.now());
+  const [currentImage, setCurrentImage] = useState<Record<string, string>>({});
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    const fetchNasaApod = async () => {
+      setLoading(true);
+      try {
+        if (!response) {
+          throw error;
+        }
+        let image = response;
+        setCurrentImage(image);
+      } catch (e) {
+        setError("There was an error. Please try again.");
+        console.error(`There was an error: ${e}`);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNasaApod();
+  }, [currentDate]);
+
+  let handlePastClick = () => {
+    let past = currentDate.minus({ days: 1 });
+    console.log(past);
+    setCurrentDate(past);
+  };
+
+  let canGoForward = () => {
+    if (currentDate > today) return false;
+  };
+
+  let handleFutureClick = () => {
+    if (!canGoForward()) return;
+    let future = currentDate.plus({ days: 1 }).day;
+    setCurrentDate(future);
+  };
 
   return (
     <>
-      <div className="m-20">Hello World</div>
+      <div className=" container flex mx-auto min-h-full py-8">
+        <Card className="max-w-md">
+          <CardHeader>
+            <CardTitle>NASA Picture of Day</CardTitle>
+
+            {currentImage.title && !loading && (
+              <>
+                <h3>{currentImage.title}</h3>
+                <button
+                  onClick={handlePastClick}
+                  className="bg-black text-white w-2 h-2"
+                >
+                  <ChevronLeftIcon />
+                </button>
+
+                <span>{`${currentDate.year}-${currentDate.month}-${currentDate.day}`}</span>
+
+                <button
+                  onClick={handleFutureClick}
+                  className="bg-black text-white w-2 h-2"
+                >
+                  <ChevronRightIcon />
+                </button>
+              </>
+            )}
+          </CardHeader>
+          {!currentImage && loading && <div>Loading...</div>}
+          {error && <div>{error}</div>}
+          {currentImage && !loading && (
+            <CardContent>
+              <img src={currentImage.url} width={500} />
+              <CardDescription className="py-2">
+                {currentImage.explanation}
+              </CardDescription>
+            </CardContent>
+          )}
+        </Card>
+      </div>
     </>
   );
 }
